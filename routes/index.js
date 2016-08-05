af5f7e9598398d04e3f4bd4f08fd3d28815f54dd
danshiner <danshiner@gmail.com>
2016-08-05 16:32:23 -0400

    Testing event response

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/listener', function(req, res, next) {
-  res.render('listener', { title: 'Listener' });
+  // res.render('listener', { title: 'Listener' });
+  res.status(200).json({ "challenge":"3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P" });
 });
 
 module.exports = router;
