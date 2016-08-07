e734ba03c68dcb0e7ccf67d6a1e02c0fa576a623
danshiner <danshiner@gmail.com>
2016-08-06 21:26:27 -0400

    replace XMLHttpRequest with node http module

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,24 +15,31 @@ router.post('/listener', function(req, res, next) {
 
 router.get('/tokenissuer', function(req, res, next) {
 
-  function httpGetAsync(url, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-        callback(xmlHttp.responseText);
-      };
-    };
-    xmlHttp.open("GET", url, true); // true for asynchronous
-    xmlHttp.send(null);
-  }
-
   let url = 'https://slack.com/api/oauth.access?client_id=66952888932.66964105143&client_secret=48a0a831119bd4acbebb71ebedb09e09&code=req.param.code'
 
-  httpGetAsync(url, function(httpResponse){
-    accessToken = httpResponse.accessToken;
-    scope = httpResponse.scope;
-    console.log("get request worked");
+  http.get('url', (res) => {
+    console.log(`Got response: ${res.statusCode}`);
+    res.resume();
+  }).on('error', (e) => {
+    console.log(`Got error: ${e.message}`);
   });
+
+  // function httpGetAsync(url, callback) {
+  //   var xmlHttp = new XMLHttpRequest();
+  //   xmlHttp.onreadystatechange = function() {
+  //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+  //       callback(xmlHttp.responseText);
+  //     };
+  //   };
+  //   xmlHttp.open("GET", url, true); // true for asynchronous
+  //   xmlHttp.send(null);
+  // }
+
+  // httpGetAsync(url, function(httpResponse){
+  //   accessToken = httpResponse.accessToken;
+  //   scope = httpResponse.scope;
+  //   console.log("get request worked");
+  // });
 
   // res.render('listener', { title: 'Listener' });
   res.status(200).json({ "challenge":req.body.challenge });
